02d7d1856ea26d25417a505e3a13ecbc7cfeb999
Arteym <a.kashipov@rt-solar.ru>
2021-10-31 20:21:42 +0300

    added interval between notif and not deleting from config

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import telebot
 from binance import AsyncClient
 
 from config import load_config, write_config
+import time
+import traceback
 
 
 class Notificator:
@@ -12,20 +14,26 @@ class Notificator:
         self.counter = {}
         self.config = load_config()
         self.bot = telebot.TeleBot(self.config["token"])
+        self.interval = self.config['interval']
 
-    def process_condition(self, condition, sub_message, ticker, price):
+    def process_condition(
+        self, condition, sup_condition, sub_message, ticker, price
+    ):
         current_symbol = ticker["symbol"]
         if condition:
-            subscribed_tickers = self.config["tickers"]
-            self.bot.send_message(
-                self.config["chat_id"],
-                text=self.text(ticker, price, sub_message),
+            self.counter[(current_symbol, price)][0] = (
+                "<" if self.counter[(current_symbol, price)][0] == ">" else ">"
             )
-            self.counter.pop((current_symbol, price))
-            subscribed_tickers[current_symbol].remove(price)
-            if len(subscribed_tickers[current_symbol]) == 0:
-                subscribed_tickers.pop(current_symbol)
-            write_config(self.config)
+            if sup_condition:
+                self.bot.send_message(
+                    self.config["chat_id"],
+                    text=self.text(ticker, price, sub_message),
+                )
+                self.counter[(current_symbol, price)][1] = time.time()
+                # subscribed_tickers[current_symbol].remove(price)
+                # if len(subscribed_tickers[current_symbol]) == 0:
+                #     subscribed_tickers.pop(current_symbol)
+                # write_config(self.config)
 
     @staticmethod
     def text(ticker, price, x):
@@ -52,40 +60,57 @@ class Notificator:
                         prices = subscribed_tickers[current_symbol]
                         for price in prices:
                             if (current_symbol, price) not in self.counter:
-                                self.counter[(current_symbol, price)] = ""
-                            if len(self.counter[(current_symbol, price)]) == 0:
+                                self.counter[(current_symbol, price)] = [
+                                    "",
+                                    None,
+                                ]
+                            if (
+                                len(self.counter[(current_symbol, price)][0])
+                                == 0
+                            ):
                                 if float(ticker["price"]) > price:
-                                    self.counter[(current_symbol, price)] = ">"
+                                    self.counter[(current_symbol, price)][
+                                        0
+                                    ] = ">"
                                 else:
-                                    self.counter[(current_symbol, price)] = "<"
-                            if len(self.counter[(current_symbol, price)]) == 1:
+                                    self.counter[(current_symbol, price)][
+                                        0
+                                    ] = "<"
+                            if (
+                                len(self.counter[(current_symbol, price)][0])
+                                == 1
+                            ):
                                 self.process_condition(
                                     float(ticker["price"]) > price
-                                    and self.counter[(current_symbol, price)]
+                                    and self.counter[(current_symbol, price)][0]
                                     == "<",
+                                    self.counter[(current_symbol, price)][1]
+                                    is None
+                                    or time.time()
+                                    - self.counter[(current_symbol, price)][1]
+                                    > self.interval,
                                     "превысила отметку",
                                     ticker,
                                     price,
                                 )
                                 self.process_condition(
                                     float(ticker["price"]) < price
-                                    and self.counter[(current_symbol, price)]
+                                    and self.counter[(current_symbol, price)][0]
                                     == ">",
+                                    self.counter[(current_symbol, price)][1]
+                                    is None
+                                    or time.time()
+                                    - self.counter[(current_symbol, price)][1]
+                                    > self.interval,
                                     "опустилась ниже отметки",
                                     ticker,
                                     price,
                                 )
-
                 await asyncio.sleep(1)
             except BaseException as ex:
-                print(ex)
+                traceback.print_exc()
                 break
         await self.client.close_connection()
-
-    async def f(self):
-        c = AsyncClient()
-        tickers = await c.get_all_tickers()
-        print(tickers)
 
 
 def main():
